1a33225e5e909a4a8106bf95c2e595fab9f3f59d
贾银河 <67949545+975904855@users.noreply.github.com>
2020-07-26 10:32:59 +0800

    判断回文
    判断语句是否为回文

---
+++ 判断回文.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int i,k;
+	char line[80];
+	printf("Enter a string:");
+	k=0;
+	while((line[k]=getchar())!='\n')
+		k++;
+	line[k]='\0';
+
+	i=0;
+	k=k-1;
+	while(i<k)
+	{
+		if(line[i]!=line[k])
+			break;
+		i++;
+		k--;
+
+	}
+	if(i>=k)
+		printf("yes");
+	else 
+		printf("No");
+}
